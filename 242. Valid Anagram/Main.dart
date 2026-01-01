c72bb72602887155eb5c7379a4e8b105e994fa0d
OnoPUNPUN <punpunonodera754@gmail.com>
2026-01-01 13:58:53 +0600

    .

--- 242. Valid Anagram/Main.dart
+++ 242. Valid Anagram/Main.dart
@@ -1,12 +1,12 @@
-void main() {}
+// void main() {}
 
-class Solution {
-  bool isAnagram(String s, String t) {
+// class Solution {
+//   bool isAnagram(String s, String t) {
 
-    for(int i = 0; i < s.length; i++){
-      if(s[i].contains(t[i])){
-        
-      }
-    }
-  }
-}
+//     for(int i = 0; i < s.length; i++){
+//       if(s[i].contains(t[i])){
+
+//       }
+//     }
+//   }
+// }
---
+++ 83. Remove Duplicates from Sorted List/Main.py
@@ -0,0 +1,8 @@
+def deleteDuplicates(self, head):
+    temp = head
+    while(temp and temp.next):
+        if(temp.next.val == temp.val):
+            temp.next = temp.next.next
+            continue
+        temp = temp.next
+    return head
